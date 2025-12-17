b7ad7335034187d53fb60da8949d8240071a2850
chandan7007 <ckr7007@gmail.com>
2025-12-17 16:51:25 +0530

    my first project based upon oops

--- oops_proj.py
+++ oops_proj.py
@@ -13,14 +13,56 @@ class chatbook:
                          4. press 4 to message a friend 
                          5. press any key to exit """)
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
-            pass
+            self.my_post()
         elif user_input=="4":
-            pass
+            self.senmsg()
         else:
             exit()
+    def signup(self):
+        email=input("enter your email here >")
+        pwd=input("setup your password here ")
+        self.username=email
+        self.password=pwd
+        print("you have signed up successfully")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname=input("enter your user name")
+            pwd=input("enter your password")
+            if self.username==uname and self.password==pwd:
+                print("you have signed up successfully")
+                self.loggedin=True
+            else:
+                print("please input correct credentials")
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin==True:
+            txt=input("enter your message here ")
+            print(f"following content has been posted -> {txt}")
+        else:
+            print("you need to signin first to post something ..")
+        print("\n")
+        self.menu()
+
+    def senmsg(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here ->")
+            frnd=input("Whom to send the msg ")
+            print(f"your message has been sent to your friend {frnd}")
+        else:
+            print("you need to sign in to send message")
+        print("\n")
+        self.menu()
+
+
         
 obj=chatbook()
